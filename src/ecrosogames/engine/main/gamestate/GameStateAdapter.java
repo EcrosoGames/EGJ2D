855e0863cc6e1d44a4e11b60d16db996fac40a5c
CoderMusgrove <codermusgrove@gmail.com>
2015-03-01 15:52:21 -0600

    Made an Adapter for the GameState class.

---
+++ src/ecrosogames/engine/main/gamestate/GameStateAdapter.java
@@ -0,0 +1,59 @@
+package ecrosogames.engine.main.gamestate;
+
+/**
+ * A simple adapter for the {@link GameState} class.
+ * 
+ * @author Michael Musgrove
+ */
+public class GameStateAdapter extends GameState {
+
+	@Override
+	public void init() {
+
+	}
+
+	@Override
+	public void load() {
+
+	}
+
+	@Override
+	public void update() {
+
+	}
+
+	@Override
+	public void render() {
+
+	}
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+}
