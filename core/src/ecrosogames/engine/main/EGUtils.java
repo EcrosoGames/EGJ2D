561e64e15fbc72f3542bd6d119ccd3d8bb393e01
CoderMusgrove <codermusgrove@gmail.com>
2015-04-26 21:20:39 -0500

    EGUtils now has access to the EGGame class. No need anymore to cast Gdx.app.getApplicationListener() to EGGame.

--- core/src/ecrosogames/engine/main/EGUtils.java
+++ core/src/ecrosogames/engine/main/EGUtils.java
@@ -1,10 +1,16 @@
 package ecrosogames.engine.main;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public final class EGUtils {
 
 	private static final SpriteBatch batch = new SpriteBatch();
+	public static final EGGame app;
+	
+	static {
+		app = (EGGame) Gdx.app.getApplicationListener();
+	}
 
 	/**
 	 * Returns the universal {@link SpriteBatch}.
